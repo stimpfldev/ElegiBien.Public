894595c6df2077d1f8e8766592481f50ce3487bf
Federico Stimpfl <stimpfldev@gmail.com>
2026-08-27 20:36:26 -0300

    Improve flooring comparison input model

--- ElegiBien.Web/Models/Flooring/FlooringComparisonViewModel.cs
+++ ElegiBien.Web/Models/Flooring/FlooringComparisonViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using ElegiBien.Application.DTOs;
 
 namespace ElegiBien.Web.Models.Flooring;
@@ -5,9 +6,21 @@ namespace ElegiBien.Web.Models.Flooring;
 public class FlooringComparisonViewModel
 {
     public Guid AnalysisId { get; set; }
+
+    [Range(0.1, 100000, ErrorMessage = "Ingresá una superficie requerida mayor que 0."),
+     Display(Name = "Superficie requerida")]
     public decimal RequiredAreaSquareMeters { get; set; }
-    public FlooringProductAlternativeDto FirstProduct { get; set; } = new();
-    public FlooringProductAlternativeDto SecondProduct { get; set; } = new();
+
+    public FlooringProductAlternativeDto FirstProduct { get; set; } = new()
+    {
+        Name = "Producto A"
+    };
+
+    public FlooringProductAlternativeDto SecondProduct { get; set; } = new()
+    {
+        Name = "Producto B"
+    };
+
     public FlooringComparisonResultDto? Result { get; set; }
     public string? ShareUrl { get; set; }
 }
