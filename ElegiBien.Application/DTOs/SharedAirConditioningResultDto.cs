4b06e4092901b2a7133341bd6d51d75363fb2c55
Federico Stimpfl <stimpfldev@gmail.com>
2026-08-28 09:46:26 -0300

    Improve shared air conditioning result details

--- ElegiBien.Application/DTOs/SharedAirConditioningResultDto.cs
+++ ElegiBien.Application/DTOs/SharedAirConditioningResultDto.cs
@@ -1,4 +1,6 @@
-﻿namespace ElegiBien.Application.DTOs;
+using ElegiBien.Domain.Enums;
+
+namespace ElegiBien.Application.DTOs;
 
 public class SharedAirConditioningResultDto
 {
@@ -7,6 +9,14 @@ public class SharedAirConditioningResultDto
     public decimal RecommendedMaximumFrigories { get; set; }
 
     public decimal IdealFrigories { get; set; }
+
+    public decimal SurfaceSquareMeters { get; set; }
+
+    public decimal VolumeCubicMeters { get; set; }
+
+    public ConfidenceLevel ConfidenceLevel { get; set; }
+
+    public bool RequiresProfessionalReview { get; set; }
 
     public List<ProductScoreResultDto> Products { get; set; } = [];
 
